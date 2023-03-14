57f682d93a4401f6e2d9cd51d8f4db53f02fdfa0
zamparg <zamparg@hotmail.com>
2023-03-14 18:09:26 -0300

    implementación inicio de sesion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes, CanActivate } from '@angular/router';
 import { BaseComponent } from './components/base/base.component';
 import { ErrorComponent } from './components/error/error.component';
-import { LoginComponent } from './components/login/login.component';
+// import { LoginComponent } from './components/login/login.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { AuthGuard } from './services/auth.guard';
 
 
 const appRoutes: Routes = [
   {path: '', component:BaseComponent, pathMatch: 'full'},
-  {path: 'login', component:LoginComponent},
+ // {path: 'login', component:LoginComponent},
   {path: 'profile', component:ProfileComponent, canActivate:[AuthGuard]},
   {path: '**', component: ErrorComponent}
 ];
